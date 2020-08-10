594b8dfc163e8eaa1914e19f0e6401671d6fab73
Sungjoon Park <sungjoon512@gmail.com>
2020-08-10 20:41:28 +0900

    Tidy up stuff

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.style.css'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Lorem Ipsum React</h1>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import './index.css'
-import App from './App'
+import './styles/index.css'
+import App from './components/app'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-)
+function RootComponent() {
+  return (
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  )
+}
+
+const $root = document.getElementById('root')
+
+ReactDOM.render(<RootComponent />, $root)
