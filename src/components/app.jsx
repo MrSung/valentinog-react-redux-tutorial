793edd139bce2f71b23616e74f4a929d45950c13
Sungjoon Park <sungjoon512@gmail.com>
2020-08-15 21:42:24 +0900

    Update UI

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,13 +6,12 @@ import Post from './posts'
 const App = () => (
   <div className="wrapper">
     <section>
-      <h2>Articles</h2>
-      <List />
-    </section>
-    <section>
       <h2>Add a new article</h2>
       <Form />
+      <h3>List</h3>
+      <List />
     </section>
+    <hr />
     <section>
       <h2>API posts</h2>
       <Post />
